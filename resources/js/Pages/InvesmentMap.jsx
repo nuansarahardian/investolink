dc1a72786c56b7c5c47977b6430fbcc13bff78b3
Aldo <aldohardika98@gmail.com>
2024-09-23 08:06:45 +0700

    footer invesment map

--- resources/js/Pages/InvesmentMap.jsx
+++ resources/js/Pages/InvesmentMap.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import NavBar from "@/Components/NavBar";
 import GeoMap from "@/Components/GeoMap";
+import Footer from "@/Components/Footer"; // Import the Footer component
 import "leaflet/dist/leaflet.css";
 
 const InvesmentMap = () => {
     return (
         <>
-            <NavBar></NavBar>
+            <NavBar />
             <div className="bg-[#FAFAFA] h-[1000px]">
                 <div className="flex flex-col justify-center align-middle place-content-center m-auto">
                     <div className="w-[95%] bg-white shadow h-full m-auto mt-10 rounded-[12px]">
                         <div className="w-[95%] m-auto mt-6 text-[#3F3F3F]">
-                            <div className="font-bold text-2xl ">
+                            <div className="font-bold text-2xl">
                                 Potensi Ekonomi Per Provinsi
                             </div>
                             <div>
@@ -23,6 +24,7 @@ const InvesmentMap = () => {
                     </div>
                 </div>
             </div>
+            <Footer /> {/* Call the Footer component here */}
         </>
     );
 };
